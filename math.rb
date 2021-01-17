b99f758a31bcc05c789545cfa4e710e665923ae7
rusislavov <rusi.slavov@gmail.com>
2021-01-17 19:52:49 +0200

    Add some simple math

---
+++ math.rb
@@ -0,0 +1,13 @@
+def add(first, second)
+  first + second
+end
+
+def multiply(first, second)
+  first * second
+end
+
+p "Look, ma, I can add!"
+p add(11, 12)
+
+p "Look, ma, I can also multiply!"
+p multiply(5, 10)
