f91e9996e2c74a2b39043d6d6a1b94eb6598d5d0
rusislavov <rusi.slavov@gmail.com>
2021-01-17 19:55:11 +0200

    Add a method that subtracts two numbers
    Since we have a method that adds two numbers, it makes sense to add one to subtract them too.

--- math.rb
+++ math.rb
@@ -6,8 +6,15 @@ def multiply(first, second)
   first * second
 end
 
+def subtract(first, second)
+  first - second
+end
+
 p "Look, ma, I can add!"
 p add(11, 12)
+
+p "I can lalso subtract!"
+p subtract(20, 14)
 
 p "Look, ma, I can also multiply!"
 p multiply(5, 10)
